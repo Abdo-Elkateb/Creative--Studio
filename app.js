fdcf5dc0978c6b6cc73714503c49f0a13b5e3eb5
Abdo Elkateb <elkatebdev@gmail.com>
2023-03-15 21:04:40 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,43 @@
+
+
+
+var main = document.getElementById("nav__main")
+
+window.onscroll = function() {
+    if(scrollY > 200) {
+        main.classList.add("nav_fixed")
+    } else {
+        main.classList.remove("nav_fixed")
+    }
+
+}
+
+
+ function about() {
+     window.scroll = 400;
+     console.log("this si")
+}
+
+
+
+
+
+
+// function service() {
+//     console.log("hi this is a about")
+// }
+// function portfollo() {
+//     console.log("hi this is a about")
+// }
+// function team() {
+//     console.log("hi this is a about")
+// }
+// function teastimonial() {
+//     console.log("hi this is a about")
+// }
+// function blog() {
+//     console.log("hi this is a about")
+// }
+// function contact() {
+//     console.log("hi this is a about")
+// }
